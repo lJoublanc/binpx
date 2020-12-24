69e0660fd696ad3725fdfbad9c52bfd68fe8c97b
Luciano Joublanc <luciano@joublanc.com>
2020-12-24 13:33:36 +0100

    Optimize using hylomorphism

--- Main.hs
+++ Main.hs
@@ -13,8 +13,6 @@ module Main where
 import Data.Functor.Foldable
 import Data.Functor.Foldable.TH
 import GHC.Float (int2Double)
-import Control.Comonad.Cofree (Cofree)
-import Control.Comonad (Comonad, Comonad(..))
 
 data Tree a 
   = Leaf a 
@@ -23,11 +21,6 @@ data Tree a
       up :: Tree a,
       down :: Tree a
     } deriving (Show, Functor)
-
-instance Comonad Tree where
-  extract (Leaf a) = a
-  extract (Branch a _ _) = a
-  duplicate x = Leaf x
 
 makeBaseFunctor ''Tree
 
@@ -65,21 +58,22 @@ main = do
     payoff = \Params{k} Seed{s} -> max 0.0 (s - k)
   }
 
+-- Calculte price using binary tree. TODO: This is ineffective as n² calculations are done.
 price :: Params -> Double
-price θ@Params{..} = histo calcPrice . (ana calcPayoff :: Seed -> Tree Double) $ Seed s_0 0.0
+price θ@Params{..} = hylo calcPrice calcPayoff $ Seed s_0 0.0
   where calcPayoff :: Seed -> Base (Tree Double) Seed
         calcPayoff Seed{t}        | t > t_T = error "δt must divide equally into T"
         calcPayoff θ_t@Seed{s, t} | t < t_T = 
           BranchF 
-            (payoff θ θ_t) --(π (u * s) (v * s))
+            (payoff θ θ_t)
             Seed {s = u * s, t = t + δt}
             Seed {s = v * s, t = t + δt}
         calcPayoff θ_t@Seed{s, t} | t == t_T = LeafF (payoff θ θ_t)
 
-        calcPrice :: Base (Tree Double) (Cofree (Base (Tree Double)) Double) ->  Double
+        calcPrice :: Base (Tree Double) Double ->  Double
         calcPrice (LeafF payoff) = payoff
-        calcPrice (BranchF _ up down) = π (extract up) (extract down) -- this is surprising: price doesn't depend on payoff at t < T.
-        π π_up π_down = disc * (p' * π_up + (1.0 - p') * π_down)
+        calcPrice (BranchF _ up down) = π up down -- this is surprising: price doesn't depend on payoff at t < T.
+        π up down = disc * (p' * up + (1.0 - p') * down)
         p' = 0.5 + r * sqrt(δt) / 2.0 / σ
         disc = 1.0 / (1.0 + r * δt)
         δt = t_T / int2Double n
